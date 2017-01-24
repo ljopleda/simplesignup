15b8d9327f6041229352e81170b54aeb3a2f7632
ljopleda <ljopleda@gmail.com>
2017-01-24 11:40:34 +0800

    Form masking & saving data

--- assets/main_script.js
+++ assets/main_script.js
@@ -1,3 +1,31 @@
 $(function(){
-	
+	$('#inp-name').keypress(function(event){
+		var regex = new RegExp("^[a-zA-Z0-9]+$");
+	    var str = String.fromCharCode(!event.charCode ? event.which : event.charCode);
+	    if (regex.test(str)) {
+	        return true;
+	    }
+	    event.preventDefault();
+	    return false;
+	});
+	$('#inp-birthday').keypress(function(event){
+		var birthday = $(this).val();
+		if(birthday.length<8){
+			var num_birthday = birthday.replace(/-/g , '');
+			if(num_birthday.length>0 && num_birthday.length%2==0){
+				$(this).val(birthday+'-');
+			}
+			var regex = new RegExp("^[0-9]+$");
+		    var str = String.fromCharCode(!event.charCode ? event.which : event.charCode);
+		    if (regex.test(str)) {
+		        return true;
+		    }
+		}
+	    event.preventDefault();
+	    return false;
+	});
+	$('#btn-export').click(function(){
+		$('input[name=action]').val('exportData');
+		$('#inp-name,#inp-birthday').attr('required',false);
+	});
 });
--- process_data.php
+++ process_data.php
@@ -1,5 +1,18 @@
 <?php
 	if($_SERVER['REQUEST_METHOD']!=='POST')
 		die('Access denied!');
-	
+	if(!empty($_POST['action'])&&!empty($_POST['name'])&&!empty($_POST['birthday'])&&!strcasecmp($_POST['action'],'saveData')){
+		if (!file_exists('data.txt')) {   
+			$file = fopen('data.txt', 'w') or die("Can't create file");   
+			fwrite($file, $_POST['name'].'|'.$_POST['birthday']."\n");     
+			fclose($file);
+		}else{
+			$file = fopen('data.txt', 'a') or die("Can't create file");   
+			fwrite($file, $_POST['name'].'|'.$_POST['birthday']."\n");     
+			fclose($file);
+		}
+		header("Location: index.php"); 
+	}else if(!empty($_POST['action'])&&!strcasecmp($_POST['action'],'exportData')){
+		echo 'export data';
+	}
 	
